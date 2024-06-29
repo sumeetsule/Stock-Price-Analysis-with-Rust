b2b271857cd02b43dda2596d5f059411747fd041
Sumeet_Sule <sule.sumeet@gmail.com>
2024-06-29 19:55:58 +0530

    Final Commit

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,24 @@ mod user;
 mod daily;
 mod test;
 
-use crate::user::{user_choice,user_symbol};
-use crate::daily::{stock_data_daily,stock_data_historical};
+use crate::user::{user_choice, user_symbol};
+use crate::daily::{stock_data_daily, stock_data_historical};
 
-/// main function
-fn main() {
+/// Main function that prompts the user for a stock symbol and time frame, 
+/// then displays the appropriate stock data.
+pub fn main() {
     let symbol = user_symbol();
     let user_choice = user_choice();
 
-    if user_choice == "Year 2024" {
-        
-        println!("Here is the 2024 stock data!");
-        stock_data_daily(&symbol);
-
-    } else if user_choice == "Historical Data" {
-        
-        println!("Here is the historical stock data!");
-        stock_data_historical(&symbol);
-
-    } else {
-        println!("Sorry could'nt process your input");
+    match user_choice.as_str() {
+        "Year 2024" => {
+            println!("Here is the 2024 stock data!");
+            stock_data_daily(&symbol);
+        },
+        "Historical Data" => {
+            println!("Here is the historical stock data!");
+            stock_data_historical(&symbol);
+        },
+        _ => println!("Sorry, couldn't process your input"),
     }
-
 }
--- src/user.rs
+++ src/user.rs
@@ -1,6 +1,24 @@
 use std::io;
 
-/// Function to get user choice for time frame
+/// Prompts the user to enter a time frame choice and returns their input as a string.
+///
+/// # Returns
+///
+/// A `String` containing the user's choice. The choices are:
+/// 
+/// - "1" for Year 2024
+/// - "2" for Historical Data
+///
+/// # Examples
+///
+/// ```
+/// let choice = user_choice();
+/// println!("User selected: {}", choice);
+/// ```
+///
+/// # Panics
+///
+/// This function will panic if it fails to read the input from the standard input stream.
 pub fn user_choice() -> String {
     println!(
         "Enter the time frequency you want to look for: 
@@ -8,15 +26,29 @@ pub fn user_choice() -> String {
             \n 1. Year 2024
             \n 2. Historical Data
             \n Enter your choice:"
-            );
+    );
 
     let mut freq_choice = String::new();
     io::stdin().read_line(&mut freq_choice).expect("Failed");
     freq_choice.trim().to_string()
 }
 
-
-/// Function to get symbol for which the user wants to see the stock data
+/// Prompts the user to enter a stock symbol and returns their input as a string.
+///
+/// # Returns
+///
+/// A `String` containing the stock symbol entered by the user.
+///
+/// # Examples
+///
+/// ```
+/// let symbol = user_symbol();
+/// println!("User entered symbol: {}", symbol);
+/// ```
+///
+/// # Panics
+///
+/// This function will panic if it fails to read the input from the standard input stream.
 pub fn user_symbol() -> String {
     println!("Enter the stock symbol you want to see:");
 
